36b537513c607eb695247ac8849007b44eaf5017
Ashlee Hart <122215203+ashbash104@users.noreply.github.com>
2026-01-12 22:07:52 -0800

    DOM Practice

---
+++ ponder/Week 2/dom.js
@@ -0,0 +1,33 @@
+const title = document.querySelector('h1');
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+document.querySelector('#topics').style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let paragraph = document.querySelector('p');
+
+paragraph.style.backgroundColor = 'lightblue';
+// Doing the same thing (diff colors) using css and just applying the class.
+paragraph.classList.add('background');
+
+//document.querySelector('body').classList.add('background');
+
+const image = document.querySelector('img');
+
+image.setAttribute('src', 'web.jpg');
+image.setAttribute('alt', 'computer code on a black screen');
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
