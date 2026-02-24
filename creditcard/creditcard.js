5cad5838e6cfe15c916bd140191bca0d2dd17bbe
Ashlee Hart <122215203+ashbash104@users.noreply.github.com>
2026-02-23 21:32:36 -0800

    credit card form

--- creditcard/creditcard.js
+++ creditcard/creditcard.js
@@ -1,1 +1,54 @@
 //Valid card number: 1234123412341234
+    const creditCardContainer = document.querySelector('#creditCardNumberContainer');
+    const creditInput = document.querySelector('#creditCardNumberContainer input');
+
+
+//validations and errors
+
+function displayError(msg) {
+	// display error message
+	document.querySelector('.errors').textContent = msg
+}
+
+function isCardNumberValid(number) {
+	return number === '1234123412341234'
+}
+
+function submitHandler(event) {
+	event.preventDefault();
+  let errorMsg = '';
+	displayError('');
+
+  let cardNumber = document.querySelector('#creditCardNumber');
+  const cardNum = cardNumber.value.trim();
+  if (paymentSelect.value === 'creditCard') {
+    // Check if it's numeric and valid in one go
+    
+      if (!/^\d{16}$/.test(cardNum)) {
+      errorMsg += 'Card number must be 16 digits\n';
+      } else if (!isCardNumberValid(cardNum)) {
+        errorMsg += 'Card number is not valid\n';
+      }
+    
+    //check date
+    const expYear = Number(document.querySelector('#year').value);   
+    const expMonth = Number(document.querySelector('#month').value);
+    const currentDate = new Date()
+
+    if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth()))
+    ) {
+        errorMsg += 'Card is expired\n';
+    }
+  }
+
+    if (errorMsg !== '') {
+		// there was an error. stop the form and display the errors.
+		displayError(errorMsg)
+		return;
+    }
+    // Success: show a confirmation message
+    const formContainer = document.getElementById('checkoutForm');
+    formContainer.innerHTML = '<h2>Thank you for providing your payment method.</h2>';
+}
+  
+document.querySelector('#checkoutForm').addEventListener('submit', submitHandler)
